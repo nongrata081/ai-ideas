53b4fa1de5a5c2fa8f1f0ad57a061e106064437a
Vahan Minasian <vaanatwork@gmail.com>
2024-10-17 21:51:33 +0000

    chore(devcontainers): implement devcontainer.json + docker-compose.yml + Dockerfile

--- .devcontainer/post_create.sh
+++ .devcontainer/post_create.sh
@@ -3,11 +3,11 @@
 set -e  # Exit immediately if a command exits with a non-zero status.
 
 # Run npm install in the workspace root
-sudo npm install --legacy-peer-deps
+npm install --legacy-peer-deps
 
 # Change directory to ai-ideas-nextra and run npm install
 cd ai-ideas-nextra
-sudo npm install
+npm install
 
 # Start the knowledge base development server
 npm run dev
