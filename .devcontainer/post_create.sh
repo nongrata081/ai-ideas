537ada25c0c41019aafbd424e85f6c6ffd15caf1
Vahan Minasian <vaanatwork@gmail.com>
2024-10-20 07:19:38 +0000

    chore(devcontainers): add github-cli install, ssh setup docs

--- .devcontainer/post_create.sh
+++ .devcontainer/post_create.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 
-set -e  # Exit immediately if a command exits with a non-zero status.
+# Exit immediately if a command exits with a non-zero status.
+set -e
+
+# # Setup ssh identity for github
+# echo -e "Host github.com\n  AddKeysToAgent yes\n  IdentityFile ~/.ssh/id_ed25519" >> ~/.ssh/config
+
+# Install nx globally
+sudo npm add --global nx@latest
 
 # Run npm install in the workspace root
 npm install --legacy-peer-deps
