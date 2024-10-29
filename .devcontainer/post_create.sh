603023a8bb8f1d95084ce3a2e01be4708aa7a7f8
Vahan Minasian <vaanatwork@gmail.com>
2024-10-29 13:17:11 +0000

    chore(pre-push): enable husky pre-push script

--- .devcontainer/post_create.sh
+++ .devcontainer/post_create.sh
@@ -9,6 +9,12 @@ sudo npm add --global nx@latest
 # Run npm install in the workspace root
 npm install --legacy-peer-deps
 
+# Install Playwright Browsers
+npx playwright install
+
+# Install Playwright Dependencies
+npx playwright install-deps
+
 # Change directory to ai-ideas-nextra and run npm install
 cd ai-ideas-nextra
 npm install
